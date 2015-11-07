7ebacb912545198adb08edce0b4f1e34287e34d5
siddhujz <kodalisiddhu@gmail.com>
2015-11-07 01:42:38 +0000

    Introduced First Name, Last Name as custom devise fields along with validation for those fields.

--- app/controllers/custom_devise/registrations_controller.rb
+++ app/controllers/custom_devise/registrations_controller.rb
@@ -25,4 +25,14 @@ class CustomDevise::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  protected
+  
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+
 end
