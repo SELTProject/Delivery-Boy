08765040fbdd09cf1184d2e4c1fff9bdd4fe30c2
siddhujz <kodalisiddhu@gmail.com>
2015-11-09 23:55:25 +0000

    Introduced email functionality so that confirmation email could be sent while signup of customers, drivers and business owners.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,4 +76,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'localdeliverysystem.herokuapp.com' }
+  
+  Rails.application.routes.default_url_options[:host] = 'localdeliverysystem.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :port           => '587',
+      :authentication => :plain,
+      #:user_name      => ENV['SENDGRID_USERNAME'],
+      #:password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => 'localdeliverysystem@gmail.com',
+      :password       => 'ldsheroku',
+      :domain         => 'gmail.com',
+      :enable_starttls_auto => true
+  }
 end
