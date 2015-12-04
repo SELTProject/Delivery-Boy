976c908963fabe1ae9ecc82af47240feb2fd14c3
siddhujz <kodalisiddhu@gmail.com>
2015-12-04 05:45:01 +0000

    Introduced fields in the table driverpickup.

--- db/migrate/20151204053351_create_driverpickups.rb
+++ db/migrate/20151204053351_create_driverpickups.rb
@@ -1,6 +1,8 @@
 class CreateDriverpickups < ActiveRecord::Migration
   def change
     create_table :driverpickups do |t|
+      t.integer :user_id
+      t.string :driver_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201034456) do
+ActiveRecord::Schema.define(version: 20151204053351) do
 
   create_table "businessdetails", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20151201034456) do
     t.integer  "user_id"
     t.string   "vehicle_no"
     t.string   "license_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "driverpickups", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "driver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
