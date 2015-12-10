9818870a7ebdb64838cb4612eab2f4398d9ab060
Balaji <bala92@gmail.com>
2015-12-10 08:36:43 -0600

    changes to migrate driver pickups

--- db/migrate/20151204053351_create_driverpickups.rb
+++ db/migrate/20151204053351_create_driverpickups.rb
@@ -2,7 +2,7 @@ class CreateDriverpickups < ActiveRecord::Migration
   def change
     create_table :driverpickups do |t|
       t.integer :customer_id
-      t.string :driver_id
+      t.integer :driver_id
       t.timestamps null: false
     end
   end
