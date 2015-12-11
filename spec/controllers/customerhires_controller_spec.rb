9bd63f41ef4a63120889b1d17c2e48d889a746d4
rishabh92 <rishabh-singh@uiowa.edu>
2015-12-11 16:51:02 +0000

    adding Rspec

--- spec/controllers/customerhires_controller_spec.rb
+++ spec/controllers/customerhires_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CustomerhiresController, type: :controller do
+     describe "Customer Hire form" do
+    it "Customer Hire new page" do
+      expect("Customerhire").to receive(:new)
+      get :new
+      expect(response).to render_template("layouts/application","customerhires/new")
+
+    end
+  end
 
 end
