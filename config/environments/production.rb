25f9e944410fb65c2be4ec0d411be4193e3bcfa7
Balaji <bala92@gmail.com>
2015-12-15 16:11:46 -0600

    host changed to deliveryboy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localdeliverysystem.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'deliveryboy.herokuapp.com' }
 
-  Rails.application.routes.default_url_options[:host] = 'localdeliverysystem.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'deliveryboy.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
